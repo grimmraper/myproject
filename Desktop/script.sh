6ecb75cf3c140de8cd01a7cf4541e9628d5c427a
Adam Hegyaljai <hegyaljai.aadam@gmail.com>
2017-05-30 14:38:22 +0200

    /home/qwe/Desktop/script.sh

---
+++ Desktop/script.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt install -y npm
+sudo apt-get install -y nodejs
+npm config set proxy http://fihelprx-defraprx.glb.nsn-net.net:8080
+npm config set https-proxy http://fihelprx-defraprx.glb.nsn-net.net:8080
+sudo npm install -g bower
+git clone https://github.com/cobudget/cobudget-ui
+cd cobudget-ui
+sudo npm install -g npm
+#nem biztos hogy kell
+npm install --save-dev gulp-install
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+#nem biztos hogy kell
+sudo npm install -g gulp
+npm install gulp
+sudo npm install -g coffee-script
+sudo npm install --save-dev coffee-script
+sudo npm install gulp-watch --save-dev
+sudo npm install vinyl-source-stream
+sudo npm install vinyl-buffer --save-dev
+sudo npm install gulp-plumber --save-dev
+sudo npm install gulp-sourcemaps --save-dev
+sudo npm install debug --save-dev
+sudo npm install xtend --save-dev
+sudo npm install lodash --save-dev
+sudo npm install gulp-livereload --save-dev
+sudo npm install gulp-sass --save-dev
+sudo npm install gulp-autoprefixer --save-dev
+sudo npm install gulp-rename --save-dev
+sudo npm install gulp-filter --save-dev
+sudo npm install browserify --save-dev
+sudo npm install gulp-nodemon --save-dev
+sudo npm install watchify --save-dev
+sudo npm install babel-register --save-dev
+sudo npm install browserify-ngannotate --save-dev
+sudo npm install babelify --save-dev
+sudo npm install envify --save-dev
+sudo npm install evalify --save-dev
+sudo npm install txtify2 --save-dev
+sudo npm install concatenify --save-dev
+sudo npm install coffeeify --save-dev
+#
+sudo npm install angular_record_store --save-dev
+sudo npm install lokijs --save-dev
+sudo npm install moment --save-dev
+sudo npm install jquery --save-dev
+sudo npm install camelize --save-dev
+sudo npm install morph --save-dev
+sudo npm install listify --save-dev
+sudo npm install is-empty-object --save-dev
+sudo npm install bowser --save-dev
+sudo npm install highcharts --save-dev
+sudo npm install angular --save-dev
+sudo npm install angular-ui-router --save-dev
+
+#sudo npm install ng-token-auth --save-dev
+sudo npm install angular-aria --save-dev
+sudo npm install angular-animate --save-dev
+sudo npm install angular-material --save-dev
+sudo npm install angular-messages --save-dev
+sudo npm install ng-focus-if --save-dev
+#sudo npm install angular-upload --save-dev
+sudo npm install angular-material-icons --save-dev
+sudo npm install ng-sanitize --save-dev
+sudo npm install angular-truncate-2 --save-dev
+#sudo npm install angular-marked --save-dev
+sudo npm install ng-q-all-settled --save-dev
+sudo npm install ng-csv --save-dev
+sudo npm install angular-chart.js --save-dev
+sudo npm install highcharts-ng --save-dev
+sudo npm install angular-material-data-table --save-dev
+#
+sudo npm install angular-sanitize --save-dev
+sudo npm install -g bower
+sudo npm install angular-cookie --save-dev
+#sudo npm install angular-autodisable/angular-autodisable --save-dev
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
